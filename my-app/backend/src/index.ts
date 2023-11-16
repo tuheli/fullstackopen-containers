c911dd6da70badbc4b63521619263f843512e0aa
tuheli <elias.tuhola@gmail.com>
2023-11-16 20:50:52 +0200

    Port is now based on process.env; Fixed routes for nginx

--- my-app/backend/src/index.ts
+++ my-app/backend/src/index.ts
@@ -3,7 +3,7 @@ import cors, { CorsOptions } from 'cors';
 import diagnosesRouter from './routes/diagnoses';
 import patientsRouter from './routes/patients';
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
@@ -11,12 +11,12 @@ app.use(express.json());
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/43909
 app.use((cors as (options: CorsOptions) => RequestHandler)({}));
 
-app.get('/api/ping', (_req, res) => {
+app.get('/ping', (_req, res) => {
   res.status(200).send('pong');
 });
 
-app.use('/api/diagnoses', diagnosesRouter);
-app.use('/api/patients', patientsRouter);
+app.use('/diagnoses', diagnosesRouter);
+app.use('/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
